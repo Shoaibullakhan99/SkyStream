8dfa88066135cac0702a3eaa4b205df58515d80b
Shoaib ulla khan <shoaibullakhan1999@outlook.com>
2024-07-05 12:49:58 +0530

    Added flights model

---
+++ SkyStream_flightService/models/Flights.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const FlightShema = mongoose.Schema({
+    flightNumber: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    departure: {
+        type: String,
+        required: true
+    },
+    destination: {
+        type: String,
+        required: true
+    },
+    departureTime: {
+        type: Date,
+        required: true
+    },
+    arrivalTime: {
+        type: Date,
+        required: true
+    },
+    aircraftId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Aircraft',
+        required: true
+    },
+    crewId: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Crew'
+    }]
+}, {timeStamps: true});
+
+module.exports = mongoose.Schema('Flight', FlightShema);
